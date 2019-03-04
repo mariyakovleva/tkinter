89c76796ec77a89c26b463863692fa71caa2234b
Кабинет информатики №310 <kab310@gym11.local>
2019-03-04 19:01:17 +0700

    first version

--- Lesson_2/040302.py
+++ Lesson_2/040302.py
@@ -2,16 +2,18 @@ from tkinter import*
 root=Tk()
 root.geometry('500x100')
 btn1=Button(root, text='Button1')
-btn1.grid(row=0)
+btn1.grid(row=0,column=0, padx=2)
 
-ent1=Entry(root)
-ent1.grid(row=0,column=2, sticky=W)
+ent1=Entry(root, width=30)
+ent1.grid(row=2, column=1,columnspan=4, sticky=S, padx=40)
 
+btn2=Button(root, text='Button2')
+btn2.grid(row=0,column=1, padx=2)          
 
-btn2=Button(root, text='Button2', height=2)
-btn2.grid(row=3,column=0, pady=10)
+btn3=Button(root, text='Button3')
+btn3.grid(row=0,column=2, padx=2)
 
-ent1=Entry(root, width=40)
-ent1.grid(row=3,column=2, sticky=N,pady=10 )
+btn4=Button(root, text='Button4')
+btn4.grid(row=0,column=3, padx=2)
 
 root.mainloop()
